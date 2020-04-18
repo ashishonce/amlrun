a33368fe0c90de966128adbdef6bf78661a837c1
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-04-18 22:53:07 +0530

    udpating

--- code/utils.py
+++ code/utils.py
@@ -108,8 +108,9 @@ def load_runconfig_python(workspace, runconfig_python_file, runconfig_python_fun
             location=runconfig_python_file
         )
         run_config_module = importlib.util.module_from_spec(spec=run_config_spec)
-        print(run_config_module)
+        print(run_config_module, runconfig_python_function_name)
         run_config_spec.loader.exec_module(run_config_module)
+        print(" loading complete")
         run_config_function = getattr(run_config_module, runconfig_python_function_name, None)
     except ModuleNotFoundError as exception:
         print(f"::debug::Could not load python script in your repository which defines the experiment config (Script: /{runconfig_python_file}, Function: {runconfig_python_function_name}()): {exception}")
